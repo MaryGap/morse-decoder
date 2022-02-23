a87a9bed6157dfd77607157ef8377b03fa131d0a
MaryGap <mariya.buben@gmail.com>
2022-02-23 12:16:32 +0300

    feat: redid the code

--- src/index.js
+++ src/index.js
@@ -1,71 +1,59 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
 };
 
 function decode(expr) {
-    MORSE_TABLE[""] = " ";
-    const array = [];
-
-  const arrayTemp = [];
-  const count = 0;
-
-  for (let i = 0; i < expr.length; i += 2) {
-    if (expr.slice(i, i + 2).includes("00")) {
-      count++;
-    } else if (expr.slice(i, i + 2).includes("10")) {
-      arrayTemp.push(".");
-      count++;
-    } else if (expr.slice(i, i + 2).includes("11")) {
-      arrayTemp.push("-");
-      count++;
-    } else {
-      count++;
+  let string = "";
+  for (let i = 0; i < expr.length; i += 10) {
+    if (typeof String.prototype.replaceAll === "undefined") {
+      String.prototype.replaceAll = function (subStr, repElem) {
+        return this.replace(new RegExp(subStr, "g"), () => repElem);
+      };
     }
-    if (count === 5) {
-      array.push(MORSE_TABLE[arrayTemp.join("")]);
-      arrayTemp = [];
-      count = 0;
-    }
+    let newString = expr.slice(i, i + 10).replaceAll("00", "")
+    .replaceAll("11", "-").replaceAll("10", ".").replace("**********", " ");
+    string += `${newString} `;
   }
-
-  return array.join("");
+  return string.split("  ").map((element) =>
+      element.split(" ").map((char) => MORSE_TABLE[char]).join("")
+    ).join(" ").trim();
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
